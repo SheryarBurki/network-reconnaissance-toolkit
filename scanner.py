db710a4645539229228af3c194447093aae86c71
SheryarBurki <sheryarkburki@gmail.com>
2026-06-10 21:44:50 +0100

    Allow user to enter target IP

--- scanner.py
+++ scanner.py
@@ -1,7 +1,19 @@
 import socket
 
-target = "127.0.0.1"
+target = input("Enter target IP: ")
 ports = [22, 80, 443, 3306, 8080]
 
+print(f"Scanning target: {target}")
+
 for port in ports:
-    print(port)
+    scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    scanner.settimeout(1)
+
+    result = scanner.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port} is open")
+    else:
+        print(f"Port {port} is closed or filtered")
+
+    scanner.close()
